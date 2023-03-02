ad772ff3a7acb3f753eb516947f6296492ff9098
Abdub2 <abdubdoyo2005@gmail.com>
2023-03-02 13:56:38 +0300

    Added functions to my components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import './App.css';
+import Homepage from '/app/Homepage.js';
+import Login from '/app/Login.js';
+import MovieList from '/app/MovieList.js';
+import Register from '../app/register';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
+
+    return (
+        <div>
+            <Routes>
+                <Route exact path= "/" element={<Homepage/>}></Route>
+                <Route exact path= "/movielist" element={<MovieList/>}></Route>
+                <Route exact path= "/login" element={<Login/>}></Route>
+                <Route exact path= "/register" element={<Register/>}></Route>
+            </Routes>
+
+        </div>
+    )
 
 }
 
